d35e5425cc7b4c4e69a1ca8c2fdb57a865fd4963
jxustcmh <jxrjcmh@163.com>
2019-03-12 13:50:22 +0800

    添加了感叹号！

--- proxy/src/main/java/com/hx/designpattern/demo02/AccessValidator.java
+++ proxy/src/main/java/com/hx/designpattern/demo02/AccessValidator.java
@@ -10,12 +10,12 @@ package com.hx.designpattern.demo02;
  */
 public class AccessValidator {
     public boolean validator(String userId) {
-        System.out.println("数据库中验证"+userId+"是否合法");
+        System.out.println("数据库中验证"+userId+"是否合法！");
         if (userId.equals("杨过")){
             System.out.println(userId+" : 登录成功");
             return true;
         }else {
-            System.out.println(userId + " : 登录失败");
+            System.out.println(userId + " : 登录失败！");
             return false;
         }
     }
